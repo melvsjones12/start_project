0dc960c1c86d2713eaad416be2650f8c219dc9aa
PrincessZelda077 <trixieaquino6@gmail.com>
2017-09-23 13:21:09 +0800

    added movement

--- assets/Scripts/Player.js
+++ assets/Scripts/Player.js
@@ -21,15 +21,69 @@ cc.Class({
         return cc.repeatForever(cc.sequence(jumpUp, jumpDown));
     },
 	
+    setInputControl: function () {
+        var self = this;
+        // add keyboard event listener
+        cc.eventManager.addListener({
+            event: cc.EventListener.KEYBOARD,
+            // When there is a key being pressed down, judge if it's the designated directional button and set up acceleration in the corresponding direction
+            onKeyPressed: function(keyCode, event) {
+                switch(keyCode) {
+                    case cc.KEY.a:
+                        self.accLeft = true;
+                        self.accRight = false;
+                        break;
+                    case cc.KEY.d:
+                        self.accLeft = false;
+                        self.accRight = true;
+                        break;
+                }
+            },
+            // when releasing the button, stop acceleration in this direction
+            onKeyReleased: function(keyCode, event) {
+                switch(keyCode) {
+                    case cc.KEY.a:
+                        self.accLeft = false;
+                        break;
+                    case cc.KEY.d:
+                        self.accRight = false;
+                        break;
+                }
+            }
+        }, self.node);
+    },	
 	
     // use this for initialization
     onLoad: function () {
+        // initialize jump action
         this.jumpAction = this.setJumpAction();
-        this.node.runAction(this.jumpAction);		
+        this.node.runAction(this.jumpAction);
+
+        // switch of acceleration direction
+        this.accLeft = false;
+        this.accRight = false;
+        // current horizontal speed of main character
+        this.xSpeed = 0;
+
+        // initialize keyboard input listener
+        this.setInputControl();	
     },
 
     // called every frame, uncomment this function to activate update callback
-    // update: function (dt) {
+    update: function (dt) {
+        // update speed of each frame according to the current acceleration direction
+        if (this.accLeft) {
+            this.xSpeed -= this.accel * dt;
+        } else if (this.accRight) {
+            this.xSpeed += this.accel * dt;
+        }
+        // restrict the movement speed of the main character to the maximum movement speed
+        if ( Math.abs(this.xSpeed) > this.maxMoveSpeed ) {
+            // if speed reaches its limit, use the max speed with current direction
+            this.xSpeed = this.maxMoveSpeed * this.xSpeed / Math.abs(this.xSpeed);
+        }
 
-    // },
+        // update the position of the main character according to the current speed
+        this.node.x += this.xSpeed * dt;
+    },
 });
